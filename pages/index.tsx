1348c923154d617ce6bff91a9395751e54ba9c0b
Lucas Pacheco <lucas@eliti.com.br>
2021-07-30 10:48:40 -0300

    scroll snap horizontal centralizado

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,49 @@
+import React from 'react'
 
-export default function Home() {
+import styled from 'styled-components'
+
+const Home: React.FC = () => {
   return (
-    <div>
-      OI
-    </div>
+    <Container>
+      <Segment>
+        <h2>1</h2>
+      </Segment>
+      <Segment>
+        <h2>2</h2>
+      </Segment>
+      <Segment>
+        <h2>3</h2>
+      </Segment>
+      <Segment>
+        <h2>4</h2>
+      </Segment>
+      <Segment>
+        <h2>5</h2>
+      </Segment>
+    </Container>
   )
 }
+
+export default Home
+
+const Container = styled.div`
+	width: 100vw;
+	height: 100vh;
+	overflow-x: scroll;
+	scroll-snap-type: x mandatory;
+  color: white;
+  background-color: oldlace;
+  display: flex;
+  align-items: center;
+  border: 10px solid red;
+`
+
+const Segment = styled.div`
+  border: 5px solid green;
+	height: 100%;
+	scroll-snap-align: center;
+	display: flex;
+	background-color: darkorchid;
+	flex: 0 0 100%;
+
+`
