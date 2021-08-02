3571deaed73937e8ea201b297aa6fe0b32079f0b
Lucas Pacheco <lucas@eliti.com.br>
2021-08-01 21:33:36 -0300

    evolução

--- pages-lib/home/Slide4.tsx
+++ pages-lib/home/Slide4.tsx
@@ -9,11 +9,11 @@ type Props = {
 export default function Slide4({x, p}:Props) {
   return <Wrapper overflow={'visible'}>
     <h1>Por isso plantamos:</h1>
-    <Body p={p} x={x} v={0}>
+    <Body p={p} x={x} v={-.5}>
       <img src='/assets/img/stand.svg' alt="body" />
-      <Line1 p={p} x={x} v={0} />
-      <Line2 p={p} x={x} v={0} />
-      <Line3 p={p} x={x} v={0} />
+      <Line1 p={p} x={x} v={-.5} />
+      <Line2 p={p} x={x} v={-.5} />
+      <Line3 p={p} x={x} v={-.5} />
     </Body>
   </Wrapper>
 }
@@ -24,7 +24,13 @@ const Body = styled.div<ParallaxProps>`
   width: 100%;
   bottom: 0;
   right: 0;
-  transform: translateX(${({x, v})=> `${x*v}px`});
+  transform: translateX(${({x, v})=> {
+    const result = x * v;
+    if (result <= 0) {
+      return `${x*0.5}px`
+    }
+    return `${x*v}px`
+  }});
   text-align: right;
   img {
     width: 50%;
@@ -37,7 +43,13 @@ const Line = styled.div<ParallaxProps>`
   background: black;
   position: absolute;
   left: 0%;
-  transform: translateX(${({x, v})=> `${x*v}px`});
+  transform: translateX(${({x, v})=> {
+    const result = x * v;
+    if (result <= 0) {
+      return `${x*0.5}px`
+    }
+    return `${x*v}px`
+  }});
 `
 
 const Line1 = styled(Line)`
